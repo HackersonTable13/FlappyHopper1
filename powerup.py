0f2c6731fa56ad89062ef7678c4288db24f23275
Barkwoof <Seamusmyanez@gmail.com>
2024-10-19 14:33:51 +0100

    changed powerup to carrot

--- powerup.py
+++ powerup.py
@@ -6,7 +6,7 @@ class Powerup(pygame.sprite.Sprite):
     def __init__(self, x, y, position):
         super().__init__()
         # Load the power-up image
-        self.image = pygame.image.load('assets/powerup/powerup.png').convert_alpha()
+        self.image = pygame.image.load('assets/powerup/carrot.png').convert_alpha()
         self.rect = self.image.get_rect()
         if position == 1:
             self.rect.bottomleft = [x - 100, y - int(pipe_gap / 2)]
